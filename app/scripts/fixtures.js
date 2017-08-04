ec96d432726a0e7d2c6124c381e1864e2147f4f0
Bob-e <bob.e.mercer@gmail.com>
2017-08-03 20:40:03 -0400

    updated small bug

--- app/scripts/fixtures.js
+++ app/scripts/fixtures.js
@@ -5,11 +5,11 @@ var albumPicasso = {
     year: '1881',
     albumArtUrl: 'assets/images/album_covers/01.png',
     songs: [
-        { title: 'Blue', duration: 161.71, audioUrl: 'assets/music/blue', number: '1' },
-        { title: 'Green', duration: 103.96, audioUrl: 'assets/music/green', number: '2'},
-        { title: 'Red', duration: 268.45, audioUrl: 'assets/music/red', number: '3'},
-        { title: 'Pink', duration: 153.14, audioUrl: 'assets/music/pink', number: '4'},
-        { title: 'Magenta', duration: 374.22, audioUrl: 'assets/music/magenta', number: '5'}
+        { title: 'Blue', duration: 161.71, audioUrl: 'assets/music/blue'},
+        { title: 'Green', duration: 103.96, audioUrl: 'assets/music/green'},
+        { title: 'Red', duration: 268.45, audioUrl: 'assets/music/red'},
+        { title: 'Pink', duration: 153.14, audioUrl: 'assets/music/pink'},
+        { title: 'Magenta', duration: 374.22, audioUrl: 'assets/music/magenta'}
     ]
 };
 
